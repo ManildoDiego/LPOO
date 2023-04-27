2db5f19f6cdec3efae930ab89f20dd558aec7d8f
ManildoDiego <diegothiry27@gmail.com>
2023-04-26 22:33:59 -0300

    firstcommit

--- Collatz.cpp
+++ Collatz.cpp
@@ -8,20 +8,26 @@ using namespace std;
 // si el número es 1, el proceso termina. De lo contrario, si es par se divide entre 2, y si es impar se multiplica por 3 y se le suma 1. 
 
 int main() {
-	int64_t n;
+	// declaro un numero de 64bits sin signo
+	uint64_t n;
 	cout << "Ingrese numero entero mayor que 1: ";
 	cin >> n;
 
-	if (n <= 1) {
+	// si el numero es < 1 significa que no puede realizar este procedimiento
+	if (n < 1) {
 		cout << "Numero invalido: " << n << endl;
 		return 1;
 	}
 
 	cout << "Sucesion:" << endl;
 	while (n != 1) {
+		// si el numero es divisible por 2 lo divido por 2
 		if ((n % 2) == 0) {
 			n /= 2;
-		} else {
+		} 
+		
+		// si no, lo multiplico por 3 y le sumo 1
+		else {
 			n = (n * 3) + 1;
 		}
 		
--- luminarias.cpp
+++ luminarias.cpp
@@ -1,23 +1,10 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-/*
-	Desde la ciudad nos han pedido que hagamos un programa que nos ayude a gestionar las luces de ciertas zonas de la ciudad, desde la ciudad nos han pedido que hagamos un programa que nos ayude a gestionar ciertas zonas de la ciudad.
-	Esta zona tiene 5 calles y en cada una de ellas hay 15 luces numeradas del 1 al 15 .
-	Con el fin de ahorrar, la ciudad ha decidido que no se van a encender todas las luces de cada calle, sino que en cada calle hay un número máximo de luces que pueden estar encendidas simultáneamente. Dichos máximos se van a almacenar en un array llamado "maxcalle".
-	El programa nos pide realizar las siguientes tareas.
-	Leer el número máximo de luces que pueden estar simultáneamente encendida en cada calle y luego si encender las luces. Se debe ingresar número de calle y número de luz.
-	No todas las calles tendrán luces encendidas. Me debe avisar en caso que se intente encender el máximo permitido para esa calle. Finaliza cuando el número de luces es 0.
-	Obtener el número de calles que tienen todas las luces apagadas.
-	Clasificar las calles según los siguientes criterios:
-	Todas las luces apagadas va a  tener que decir a oscuras.
-	Si hay encendidas tantas como indica su máximo de calle y hay más encendidas que apagadas debe decir muy luminosas.
-	Si hay encendidas tanto como indica su máximo de calle pero hay más o igual número de apagadas que encendidas el cartel debe decir luminosa
-	En cualquier otro caso la respuesta es tenebrosa
-*/
-
-
+const int NUM_CALLES = 5;
+const int NUM_LUCES = 15;
 
 int main() {
 	
